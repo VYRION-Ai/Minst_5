e6e306f4dfc48fbdc3929fa960437f24b4125d33
totoadel <mh_882005@yahoo.com>
2021-08-06 21:18:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import yaml
 import ast
 print(torch.__version__)
 cd /content
-curl -L "https://www.dropbox.com/s/0da6paqyt6jg0x1/video2.zip?dl=0" > video2.zip; unzip video2.zip; 
-curl -L "https://www.dropbox.com/s/6zu18goqoqqitlr/best.pt?dl=0" > best.zip; unzip best.zip; 
-curl -L "https://www.dropbox.com/s/oovmwed5zotp554/yaml8.zip?dl=0" > yaml8.zip; unzip yaml8.zip; rm yaml8.zip
+
 
 video = YouTubeVideo("pGgM3c1e8vQ", width=500)
 display(video)
