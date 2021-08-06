024245c9b928252d9283fc8b28e398a910446e81
totoadel <mh_882005@yahoo.com>
2021-08-06 21:20:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from IPython.display import YouTubeVideo, display
 import yaml
 import ast
 print(torch.__version__)
-cd /content
-
-
 video = YouTubeVideo("pGgM3c1e8vQ", width=500)
 display(video)
 
