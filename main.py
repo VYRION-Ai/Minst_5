4c8f66b871049dfe9cd3f3e73df18b344fbfd0a6
totoadel <mh_882005@yahoo.com>
2021-08-06 21:22:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from IPython.display import Image, clear_output  # to display images
 from IPython.display import YouTubeVideo, display
 import yaml
 import ast
-print(torch.__version__)
-video = YouTubeVideo("pGgM3c1e8vQ", width=500)
-display(video)
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+streamlit run first_app.py
 
