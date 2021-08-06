54330a100de202357fe94fa343b59b1d1ec327f4
totoadel <mh_882005@yahoo.com>
2021-08-06 21:50:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import os
 cwd = os.getcwd()
 st.title(cwd)
 # Print the current working directory
-print("Current working directory: {0}".format(cwd))
+st.print("Current working directory: {0}".format(cwd))
 
 # Print the type of the returned object
-print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
+st.print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
