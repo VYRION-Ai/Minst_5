17f31a8b831905dec4540a98dfcb89e5fdee8e10
totoadel <mh_882005@yahoo.com>
2021-08-20 07:33:28 -0700

    Add files via upload

--- save_firebase.py
+++ save_firebase.py
@@ -42,10 +42,10 @@ config = {
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
 
-def save1(date_,url, number):
+def save1(date_,imgg,url, number):
     args = parser.parse_args()
     #my_image = args.source
-    storage.child("Detected").child("IMG").put(url)
+    storage.child("Detected").child(url).put(imgg)
     #time.sleep(1)
     users_ref.push().set({
         'number': number,
